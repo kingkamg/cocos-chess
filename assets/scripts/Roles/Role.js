222ffda32d8cf1e9858720f7938464bb553ef14f
Trinh7293 <trinh7293@gmail.com>
2018-01-30 17:10:59 +0700

    dev role

--- assets/scripts/Roles/Role.js
+++ assets/scripts/Roles/Role.js
@@ -2,4 +2,28 @@ class Role {
     constructor(self) {
         this.self = self
     }
+
+    _changeCoordinate(x, y, i, j) {
+        x += i
+        y += j
+        if (!this._checkInsideBoard(x, y)) return null
+        let coordinateItem = this.board.coordinateItems[x][y]
+        let chosenItem = this.board.chosenItem
+        if (this._checkNotDuplicateTeam(chosenItem, coordinateItem))
+            return [x, y]
+        else
+            return null
+    },
+
+    _checkInsideBoard(i, j) {
+        return i >= 0 && i <= 7 && j >= 0 && j <= 7
+    },
+
+    _checkNotDuplicateTeam(item1, item2) {
+        let isValid = true
+        if (item2 && item1.team === item2.team) isValid = false
+        return isValid
+    }
 }
+
+export default Role
--- assets/scripts/Roles/Xe.js
+++ assets/scripts/Roles/Xe.js
@@ -1,4 +1,19 @@
 import Role from 'Role'
 class Xe extends Role{
+    constructor() {
+        this.self = super.self
+    }
 
+    _getHighLightCoordinates() {
+        let self = this.self
+        let {_x: x, _y: y} = self
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                if (i * j != 0 || i + j === 0) continue
+                let item = this._changeCoordinate(x, y, i, j)
+                item && self.board.highlightCoordinates.push(item)
+            }
+        }
+
+    },
 }
